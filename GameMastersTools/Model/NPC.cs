f2a97ada0a653f5c66a8e927b18df254d2f3f4be
Rasmus Drotved Laursen <rasm781y@edu.easj.dk>
2019-05-14 13:00:57 +0200

    Set up UI for NPCS

--- GameMastersTools/Model/NPC.cs
+++ GameMastersTools/Model/NPC.cs
@@ -8,16 +8,25 @@ namespace GameMastersTools.Model
 {
     class NPC : BaseProperties
     {
-        public string Race { get; set; }
+
+        public string NPCName { get; set; }
+
+        public string NPCDescription { get; set; }
+
+        public int UserId { get; set; }
+
+        public NPC(string npcName, string npcDescription, int userId)
+        {
+            NPCName = npcName;
+            NPCDescription = npcDescription;
+            UserId = userId;
+        }
 
         //TODO 2 pictures
 
-        public NPC(string name, string race)
+        public override string ToString()
         {
-            Name = name;
-            Race = race;
+            return $"{nameof(NPCName)}: {NPCName}, {nameof(NPCDescription)}: {NPCDescription}, {nameof(UserId)}: {UserId}";
         }
-        
-        
     }
 }
---
+++ GameMastersTools/ViewModel/NPCViewModel.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using GameMastersTools.Annotations;
+using GameMastersTools.Model;
+
+namespace GameMastersTools.ViewModel
+{
+    class NPCViewModel : INotifyPropertyChanged
+    {
+        #region Backing Fields
+        
+        private string _description;
+        private string _name;
+        private ObservableCollection<NPC> _npCs;
+
+        #endregion
+
+
+        #region Properties
+        
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;  
+                OnPropertyChanged();
+            }
+
+        }
+
+        public string Description
+        {
+            get => _description;
+            set
+            {
+                _description = value; 
+                OnPropertyChanged();
+            }
+        }
+
+        public ObservableCollection<NPC> NPCs
+        {
+            get => _npCs;
+            set
+            {
+                _npCs = value; 
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+
+        #region MyRegion
+
+        public NPCViewModel()
+        {
+            
+            NPCs = new ObservableCollection<NPC>();
+
+            NPCs.Add(new NPC("Danny Boy","This Dude Sucks", UserViewModel.LoggedInUserId));
+            NPCs.Add(new NPC("Danny Boy", "This Dude Sucks", UserViewModel.LoggedInUserId));
+            NPCs.Add(new NPC("Danny Boy", "This Dude Sucks", UserViewModel.LoggedInUserId));
+            NPCs.Add(new NPC("Danny Boy", "This Dude Sucks", UserViewModel.LoggedInUserId));
+            NPCs.Add(new NPC("Danny Boy", "This Dude Sucks", UserViewModel.LoggedInUserId));
+            NPCs.Add(new NPC("Danny Boy", "This Dude Sucks", UserViewModel.LoggedInUserId));
+            NPCs.Add(new NPC("Danny Boy", "This Dude Sucks", UserViewModel.LoggedInUserId));
+            NPCs.Add(new NPC("Danny Boy", "This Dude Sucks", UserViewModel.LoggedInUserId));
+            NPCs.Add(new NPC("Danny Boy", "This Dude Sucks", UserViewModel.LoggedInUserId));
+            NPCs.Add(new NPC("Danny Boy", "This Dude Sucks", UserViewModel.LoggedInUserId));
+            NPCs.Add(new NPC("Danny Boy", "This Dude Sucks", UserViewModel.LoggedInUserId));
+            NPCs.Add(new NPC("Danny Boy", "This Dude Sucks", UserViewModel.LoggedInUserId));
+            NPCs.Add(new NPC("Danny Boy", "This Dude Sucks", UserViewModel.LoggedInUserId));
+        }
+
+        #endregion
+
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
